a482cbceb4e08b0717fc41e8f60a628b221a71ea
Alexey Chervenko <83578747+alexchervenko@users.noreply.github.com>
2026-05-29 09:41:18 +0300

    Update rcc-api/src/main/java/io/student/rococo/config/ServiceLocatorConfig.java
    Co-authored-by: Nikita Starkov <99817031+LuroyJenkins@users.noreply.github.com>

--- rcc-api/src/main/java/io/student/rococo/config/ServiceLocatorConfig.java
+++
@@ -1,8 +0,0 @@
-package io.student.rococo.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ServiceLocatorConfig {
-    // ServiceLocator теперь является @Component и будет создан Spring'ом автоматически
-}
